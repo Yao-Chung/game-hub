1222059a13f3eb0cb5d42430a5d084253a4f8f1c
Yao-Chung <yaochung9744@gmail.com>
2023-10-29 16:08:42 -0700

    display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,25 @@
+import { Text, HStack, Image, List, ListItem } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
 
   return (
-    <ul>
-      {data.map((d) => (
-        <li key={d.id}>{d.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} padding="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
