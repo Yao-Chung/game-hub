3be8932a63889d35f5397740093b084f3f6ca26f
Yao-Chung <yaochung9744@gmail.com>
2023-11-25 11:37:56 -0800

    implement genre with react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,3 +1,7 @@
+import { FetchContext } from "./../../node_modules/@tanstack/query-core/src/query";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 import genres from "../data/genres";
 export interface Genre {
   id: number;
@@ -5,8 +9,15 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => {
-  return { data: genres, isLoading: false, error: null };
-};
+// In this setting, the genre list will first intialize with static data
+// After 24 hours staletime, it will start fetching from backend
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: genres.length, results: genres }, // this json object should match with the one in get method
+  });
 
 export default useGenres;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -10,11 +11,12 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryClient}>
         <App />
-      </ChakraProvider>
-    </QueryClientProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
