78f83f443f0665dd00452ab21db004f2b588c052
Yao-Chung <yaochung9744@gmail.com>
2023-12-01 18:12:36 -0800

    Refactor: create reusable api client

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,19 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import apiClient, { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
+const apiClient = new APIClient<Genre>("/genres");
+
 // In this setting, the genre list will first intialize with static data
 // After 24 hours staletime, it will start fetching from backend
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
     initialData: { count: genres.length, results: genres }, // this json object should match with the one in get method
   });
