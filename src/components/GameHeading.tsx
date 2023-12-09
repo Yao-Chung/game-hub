4b2b99601f792dcdc591ec724e89ce2aba87d641
Yao-Chung <yaochung9744@gmail.com>
2023-12-08 23:30:01 -0800

    Refactor: simplify platform in GameQuery

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,6 +1,7 @@
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
 import useGenres from "../hooks/useGenres";
+import usePlatforms from "../hooks/usePlatforms";
 
 interface Props {
   gameQuery: GameQuery;
@@ -15,9 +16,13 @@ const GameHeading = ({ gameQuery }: Props) => {
   // We need to find the selected genre by its id since gameQuery didn't store the whole genre
   const { data: genres } = useGenres();
   const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
-  const heading = `${gameQuery.platform?.name || ""} ${
-    genre?.name || ""
-  } Games`;
+
+  const { data: platforms } = usePlatforms();
+  const platform = platforms?.results.find(
+    (p) => p.id === gameQuery.platformId
+  );
+
+  const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
 
   return (
     <Heading as="h1" marginY={5} fontSize="5xl">
