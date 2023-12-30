00c33b893b0577702f42909c6d8a647acc414086
Yao-Chung <yaochung9744@gmail.com>
2023-12-30 13:49:16 -0800

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,10 +7,12 @@ import { Platform } from "./usePlatforms";
 export interface Game {
   id: number;
   name: string;
+  slug: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
+  description_raw: string; // Not found in https://api.rawg.io/docs/#operation/games_read
 }
 
 const apiClient = new APIClient<Game>("/games");
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  // The code will be executed only if the route was succeed
+  // We can ensure that the undefined will not happen here: slug! tells compiler it's not null
+  const { data: game, error, isLoading } = useGame(slug!);
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (error || !game) {
+    throw error;
+  }
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+    </>
+  );
 };
 
 export default GameDetailPage;
