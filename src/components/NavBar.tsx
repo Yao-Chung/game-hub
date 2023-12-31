30c9a4400b15fe127311cc2ab65ba7559782bc0d
Yao-Chung <yaochung9744@gmail.com>
2023-12-30 21:06:16 -0800

    fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,16 @@
 import { HStack, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const Navbar = () => {
+  // objectFit property prevent image from being out of shape
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Link to={"/"}>
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,19 +1,23 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import useGameQueryStore from "../store";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
-  const setSearchText = useGameQueryStore((s) => s.setSearchText);
   // (s) => s.setSearchText means: this component will only depend on setSearchText
   // Other change will not trigger re-render
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  // Navigate the user to the main page, so user can see search text
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) {
           setSearchText(ref.current.value);
+          navigate("/");
         }
       }}
     >
